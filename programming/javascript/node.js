a829c5cbaea67d256f28759d4f8961a58ce83df8
nonsguar-go <168914579+nonsugar-go@users.noreply.github.com>
2024-05-22 23:03:27 +0900

    Create node.js

---
+++ programming/javascript/node.js
@@ -0,0 +1,16 @@
+# Node.js
+# 概要
+- https://nodejs.org/
+- https://www.tohoho-web.com/ex/nodejs.html
+## VSCode
+- VS Code ES7+ React/Redux/React-Native/JS snippets
+## my-app
+- https://github.com/npm/cli/issues/6971
+- https://stackoverflow.com/questions/66813198/react-app-localhost3000-logo-not-spinning
+
+```powershell
+PS> npm install -g npm@latest
+PS> npx create-react-app my-app
+PS> cd my-app
+PS> npm start
+```
