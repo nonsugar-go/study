712c48f9e2f8cc2fff3fac4d2f57c52afe6e708c
nonsguar-go <168914579+nonsugar-go@users.noreply.github.com>
2026-06-30 23:33:19 +0900

    Create expect.go

---
+++ programming/go/expect.go
@@ -0,0 +1,5 @@
+# Expect
+
+- https://github.com/google/goexpect
+- https://github.com/netflix/go-expect
+- [Go言語でtelnetを自動化してルータの設定を行う #Network - Qiita](https://qiita.com/jh1vxw/items/f3219cb3fe79d0a1ba67)
